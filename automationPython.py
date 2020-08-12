13b2ea7d5b8baf1e0409ac0917b2417787ed483d
Trey Hamilton <49693948+treyhamilton@users.noreply.github.com>
2020-08-12 18:15:16 -0400

    Update automationPython.py
    -created first test case
    -checks if webpage opened was the correct one, if so returns successful, if not returns failure with given outputs

--- automationPython.py
+++ automationPython.py
@@ -3,12 +3,19 @@ from selenium import webdriver
 driver = webdriver.Safari()
 driver.maximize_window()
 driver.get('https://www.google.com')
+actualTitle = str(driver.title)
+expectedTitle = "google"
 
 searchBox = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
 searchBox.send_keys('Hello, this is automated testing with Selenium')
 
 searchButton = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[3]/center/input[1]')
 searchButton.click()
+
+if actualTitle.lower() == expectedTitle.lower():
+    print("Automation Test Successful")
+else:
+    print("Automation Test Failure","\n","Actual title:",actualTitle,"\n","Expected title:",expectedTitle)
 
 driver.close()
 
